6b776d63125896c141d28c15f1e4c2ef78c5dcd2
Julia Swietochowska <julkaswietochowska@gmail.com>
2022-03-23 15:57:41 +0000

    start working on recipe details page

--- app/src/main/java/com/example/coffeeapp/RecipeDetails.java
+++ app/src/main/java/com/example/coffeeapp/RecipeDetails.java
@@ -1,4 +1,17 @@
 package com.example.coffeeapp;
 
-public class RecipeDetails {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipeDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_details);
+
+
+
+    }
 }
